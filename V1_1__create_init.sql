ee1a5f43b80081c9a31438abe93078196f651f09
Bruno Silva <bruno.tasc@gmail.com>
2021-08-30 11:05:04 -0300

    v1.0 Flyway

---
+++ V1_1__create_init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `FDM`.`persons` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL,
+  `end` VARCHAR(255) NULL,
+  `estado` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
---
+++ V1_2__populate_init.sql
@@ -0,0 +1,1 @@
+INSERT INTO `FDM`.`persons` (`nome`, `end`, `estado`) VALUES ('bruno', 'silva 17', 'rj');
